cca0bfb3d53501531965e181acc4808e8e672b50
Jonathan <jonathandscoutinho@gmail.com>
2023-11-15 22:35:55 -0300

    update 15-11 v2

--- projetoAPI/src/main/java/br/com/criandoapi/projetoAPI/model/Usuario.java
+++ projetoAPI/src/main/java/br/com/criandoapi/projetoAPI/model/Usuario.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -19,16 +22,23 @@ public class Usuario {
 	@Column(name = "id")
 	private Integer id;
 	
-	@Column(name = "nome", length = 200, nullable = true)
+
+	@Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres!")
+	@NotBlank(message = "O nome é obrigatório!")
+	@Column(name = "nome", length = 200, nullable = false)
 	private String nome;
 	
-	@Column(name = "email", length = 50, nullable = true)
+	@Email(message = "Insira um email válido!")
+	@NotBlank(message = "O email é obrigatório!")
+	@Column(name = "email", length = 50, nullable = false)
 	private String email;
 	
-	@Column(name = "senha", columnDefinition = "TEXT", nullable = true)
+	@NotBlank(message = "A senha é obrigatória!")
+	@Column(name = "senha", columnDefinition = "TEXT", nullable = false)
 	private String senha;
 	
-	@Column(name = "telefone", length = 15, nullable = true)
+	@NotBlank(message = "O telefone é obrigatório!")
+	@Column(name = "telefone", length = 15, nullable = false)
 	private String telefone;
 
 }
